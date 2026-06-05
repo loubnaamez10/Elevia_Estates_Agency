0ca2c8e4f60ab01dacbee0f66484e41b5c83fa67
Lina <lina.loubnaa@gmail.com>
2026-06-05 15:35:25 +0100

    header amelioration

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,73 @@
 import { useState } from 'react'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 import './Header.css'
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false)
+  const location = useLocation()
+
+  const closeMenu = () => setIsOpen(false)
+
+  const isHome = location.pathname === '/'
 
   return (
     <header className="header">
       <div className="container header__inner">
-        <a href="/" className="header__logo">
+        <Link to="/" className="header__logo" onClick={closeMenu}>
           ÉLÉVIA
-        </a>
+        </Link>
 
-        <nav className={`header__nav ${isOpen ? 'header__nav--open' : ''}`}>
-          <a href="/properties" onClick={() => setIsOpen(false)}>
+        <nav
+          id="site-navigation"
+          className={`header__nav ${isOpen ? 'header__nav--open' : ''}`}
+        >
+          <NavLink to="/" end className="header__link" onClick={closeMenu}>
+            Accueil
+          </NavLink>
+
+          <NavLink to="/properties" className="header__link" onClick={closeMenu}>
             Propriétés
-          </a>
-          <a href="#about" onClick={() => setIsOpen(false)}>
-            Agence
-          </a>
-          <a href="#contact" onClick={() => setIsOpen(false)}>
-            Contact
-          </a>
+          </NavLink>
+
+          {isHome ? (
+            <>
+              <a href="#about" className="header__link" onClick={closeMenu}>
+                Agence
+              </a>
+              <a href="#contact" className="header__link" onClick={closeMenu}>
+                Contact
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/#about" className="header__link" onClick={closeMenu}>
+                Agence
+              </Link>
+              <Link to="/#contact" className="header__link" onClick={closeMenu}>
+                Contact
+              </Link>
+            </>
+          )}
         </nav>
 
         <div className="header__actions">
-          <a href="#contact" className="header__cta">
-            Prendre rendez-vous
-          </a>
+          {isHome ? (
+            <a href="#contact" className="header__cta" onClick={closeMenu}>
+              Prendre rendez-vous
+            </a>
+          ) : (
+            <Link to="/#contact" className="header__cta" onClick={closeMenu}>
+              Prendre rendez-vous
+            </Link>
+          )}
 
           <button
             className="header__toggle"
             type="button"
-            onClick={() => setIsOpen(!isOpen)}
-            aria-label="Ouvrir le menu"
+            onClick={() => setIsOpen((prev) => !prev)}
+            aria-label={isOpen ? 'Fermer le menu' : 'Ouvrir le menu'}
+            aria-expanded={isOpen}
+            aria-controls="site-navigation"
           >
             <span></span>
             <span></span>
